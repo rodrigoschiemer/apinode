97e3a5fae4137e5de3be1f9b35471097fb00be7f
rodrigoschiemer <rodrigoschie@gmail.com>
2026-02-03 10:23:43 -0300

    Refatora estrutura do projeto para pasta src

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,10 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({
+	path: path.resolve(__dirname, '../../.env')
+});
+
+//require('dotenv').config();
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
