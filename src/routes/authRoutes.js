98ff6cda43017bc803ff2dc18ba7430924b6605b
Rodrigo Schiemer <02660918@rio.rj.gov.br>
2026-05-27 18:55:33 -0300

    Implementação de Rate Limit no Login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import * as authController from '../controllers/authController.js';
+import { loginLimiter } from '../middlewares/rateLimitMiddleware.js';
 
 const router = Router();
 
-router.post('/login', authController.login);
+router.post('/login', loginLimiter, authController.login);
 
 export default router;
