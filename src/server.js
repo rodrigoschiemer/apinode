3f59196ee21a96c973190a3f3e66610fee564560
Rodrigo Schiemer <02660918@rio.rj.gov.br>
2026-06-08 18:41:32 -0300

    Tradução de mensagem de conexão.

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ async function start(){
 	await testConnection();
 
 	app.listen(process.env.PORT || 3000, () => {
-		console.log(`Server running on port ${process.env.PORT || 3000}`);
+		console.log(`Servidor está rodando na porta ${process.env.PORT || 3000}`);
 	});
 
 }
